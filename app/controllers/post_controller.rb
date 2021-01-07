783acf5ea76e99e22601291430b6d2011f923e80
alfredopl <aalfredoplz@gmail.com>
2021-01-07 09:11:30 -0300

    Post controller

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,16 @@
+class PostController < ApplicationController
+  http_basic_authenticate_with name: "desafiovamoscontodo", password: "XAHTJEAS23123%23", only: :dashboard
+  
+  def index
+    @posts = Post.all.order(created_at: :desc)
+  end
+  
+  def dashboard
+  end
+
+  def create
+    @post = Post.create(title: params[:title],
+    image_url: params[:image_url],
+    content: params[:content])
+  end
+end
