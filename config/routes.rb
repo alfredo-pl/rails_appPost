9716e2d30f0757521a4790d8ef279ddcad4a2bcf
alfredopl <aalfredoplz@gmail.com>
2021-01-07 09:12:40 -0300

    routes for app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'post/index'
+  get 'post/dashboard'
+  get 'post/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'post#index'
 end
